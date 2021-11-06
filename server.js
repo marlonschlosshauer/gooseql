9b9e78549014ec2d6516c9593b2351e5ab4ee007
marlonschlosshauer <marlonschlosshauer@gmail.com>
2021-11-06 17:26:58 +0100

    initial example

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
 const express = require('express');
+const { graphqlHTTP } = require("express-graphql");
+
+const {
+	GraphQLSchema,
+	GraphQLObjectType,
+	GraphQLString
+} = require('graphql');
+
+const schema = new GraphQLSchema({
+	query: new GraphQLObjectType({
+		name: 'World',
+		fields: () => ({
+			terrain: {
+				type: GraphQLString,
+				resolve: () => 'Water'
+			}
+		})
+	})
+})
+
+
 const app = express();
+
+
+app.use('/q', graphqlHTTP({
+	schema: schema,
+	graphiql: true
+}));
 
 app.listen(5000, () => console.log('Server is running'));
